4080ca2df33a4d2e45d7bbeb32e6ca8c1c24ccbc
Tom Syroid <tsyroid@gmail.com>
2024-04-19 07:51:37 -0700

    Pad out `lua/plugins`

---
+++ lua/core/init.lua
@@ -0,0 +1,11 @@
+-- lua/core/init.lua
+-- Initialize (and ensure startup order) lua/core
+
+require('core.globals')
+require('core.options')
+require('core.keymaps')
+require('core.autocommands')
+require('core.lazy')
+
+
+-- EOF core/init.lua
