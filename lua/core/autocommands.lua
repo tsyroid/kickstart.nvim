89c8478cf755b3bbf491717f2c9acc4b71f6c836
Tom Syroid <tsyroid@gmail.com>
2024-04-19 10:47:05 -0700

    Cleanup `core/autocommands.lua`; add command to disable continuous commenting

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,16 +1,22 @@
 -- lua/core/autocommands.lua
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+-- Basic Autocommands
+-- See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- Disable new line after comment - all files
+vim.api.nvim_create_autocmd('Filetype', {
+  pattern = '*',
+  callback = function()
+    vim.opt_local.formatoptions:remove { 'r', 'o' }
   end,
 })
 
