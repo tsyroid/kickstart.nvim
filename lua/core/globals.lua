40e64766fb7885597a53d4fb49c956c47ad8e28d
Tom Syroid <tsyroid@gmail.com>
2024-04-19 10:52:51 -0700

    Cleanup `core/globals.lua`

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -1,10 +1,14 @@
 -- lua/core/globals.lua
+-- Settings with global reach go here
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
+
+-- EOF core/globals.lua
